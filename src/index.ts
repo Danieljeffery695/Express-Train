6d68b78cb40bc2637f8fa79623a8aaea301c0873
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-12-02 14:19:36 -0800

    Creating Connection and send data to the database

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import dotenv from "dotenv";
-import fs from "fs";
-
 dotenv.config({ path: "./../config.env" });
-
 import express, { type Request, type Response } from "express";
+import dbConnection from "./Db/dbConnect";
+import { createUser } from "./Controllers/CreateUsers_controller";
+
 
 const app = express();
 app.use(express.json());
-
-const simpleData = fs.readFileSync(`../simple_admin_data.json`, "utf-8");
+dbConnection();
 
 app.get("/", (req: Request, res: Response) => {
 	res.status(200).send("simple");
 });
 
+app.post("/create/user", createUser);
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
-	console.log("I have to focus on my projects");
+	console.log("I have to focus on my projects ", port);
 });
 
 export default app;

6f6aef4f42495b4172639a8a8abf7f26bb8bbb65
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-12-05 14:18:15 -0800

    Adding extra security

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,39 @@
 import dotenv from "dotenv";
+
 dotenv.config({ path: "./../config.env" });
+
 import express, { type Request, type Response } from "express";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 import dbConnection from "./Db/dbConnect";
-import { createUser } from "./Controllers/CreateUsers_controller";
+import loginAuth from "./middlewares/login_middleware";
+import router from "./routes/routers";
 
-
+// Global Middlewares 
 const app = express();
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 10 minutes
+	limit: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+});
+
 app.use(express.json());
+app.use(helmet());
+app.use(limiter);
 dbConnection();
 
+// Local routes
 app.get("/", (req: Request, res: Response) => {
 	res.status(200).send("simple");
 });
 
-app.post("/create/user", createUser);
+// Custom routes and Middlewares
+app.use(
+	loginAuth,
+	router
+);
 
 const port = process.env.PORT;
 

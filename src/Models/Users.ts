68c66bc689c5d2f6463368beccedf8c7ee6e7abd
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-12-07 09:50:00 -0800

    encrypting password and adjusting schema data

--- src/Models/Users.ts
+++ src/Models/Users.ts
@@ -1,33 +1,68 @@
-import mongoose from "mongoose";
+import mongoose, {
+   HydratedDocument
+} from "mongoose";
+import bcrypt from "bcryptjs";
+import { IUserCreate } from "../Utils/DataChecking";
 
-const userSchema = new mongoose.Schema({
-    "name": {
-        type: String,
-        require: [true, "Please Your name is require"]
+const userSchema = new mongoose.Schema<IUserCreate>({
+  name: {
+    type: String,
+    required: [true, "Please Your name is required"], 
+	minlength: 2
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please Your email is required"], 
+  },
+
+  phone: {
+    type: String,
+    required: [true, "Please Your phone number is required"], 
+  },
+
+  phoneNumberRegion: {
+	type: String,
+	default: "none",
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please provide a password"], 
+	minlength: [8, "Password length must be above eight Characters"]
+  },
+
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password for validation"], 
+    validate: {
+      validator: function (this: HydratedDocument<User>, data: string) {
+        return data === this.password;
+      },
+	  message: "Passwords are not the same!"
     },
+  },
 
-    "email": {
-        type: String,
-        unique: true,
-        require: [true, "Please Your email is require"]
-    },
-
-    "phone": {
-        type: Number,
-        require: [true, "Please Your phone number is require"]
-    }, 
-
-    "password": {
-        type: String,
-        require: [true, "Please provide a password"]
-    }, 
-
-    "isAdmin": {
-        type: Boolean,
-        default: false
-    }
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-const Users = mongoose.model("Users", userSchema);
 
-export default Users
+userSchema.pre(
+  "save",
+  async function () {
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+
+  }
+);
+
+const Users = mongoose.model<IUserCreate>("Users", userSchema);
+
+export default Users;

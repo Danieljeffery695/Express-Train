fe259da094f44f45af2bf08b879cbb98603bc8b6
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-12-08 16:05:40 -0800

    Fixing some minors issues with our routes

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,17 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./../config.env" });
 
 import express, { type Request, type Response } from "express";
-import helmet from "helmet";
 import { rateLimit } from "express-rate-limit";
+import helmet from "helmet";
 import dbConnection from "./Db/dbConnect";
-import loginAuth from "./middlewares/login_middleware";
-import router from "./routes/routers";
+import userRouter from "./routes/userRoutes";
 
-// Global Middlewares 
+// Global Middlewares
 const app = express();
 const limiter = rateLimit({
 	windowMs: 10 * 60 * 1000, // 10 minutes
 	limit: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
 });
@@ -30,10 +29,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 // Custom routes and Middlewares
-app.use(
-	loginAuth,
-	router
-);
+app.use(userRouter);
 
 const port = process.env.PORT;
 

c1157941b864a6f305a44bc154266223c520cbfd
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-12-12 16:17:30 -0800

    Authorize controller

---
+++ src/Controllers/AuthController.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, {GetPublicKeyOrSecret} from "jsonwebtoken";
+import { handleAsyncErr } from "../Utils/AsyncError";
+import catchError from "../Utils/CatchError";
+
+if(process.env.JWT_SECRET) var jwt_secret: string | Buffer<ArrayBufferLike> | GetPublicKeyOrSecret = process.env.JWT_SECRET;
+
+export const protectRoutes = handleAsyncErr( async (req: Request, res: Response, next: NextFunction) => {
+    let token;
+    const {authorizationtoken} = req.headers;
+    if(authorizationtoken && authorizationtoken.includes("Bearer", 0)) token = authorizationtoken.toString().split(' ')[1];
+    if(token) {
+        const tokenDecoder = await jwt.verify(token, jwt_secret); 
+        next();
+    }
+    
+    catchError(new Error, "Not authorize", 401, next);
+});

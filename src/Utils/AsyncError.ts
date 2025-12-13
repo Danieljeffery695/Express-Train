514f357a9a811904ad215f383daa669bcbf7946b
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-12-12 16:10:29 -0800

    Adding some suggestions

--- src/Utils/AsyncError.ts
+++ src/Utils/AsyncError.ts
@@ -10,3 +10,9 @@ export const handleAsyncErr = (fn: Function) => {
         });
     };
 };
+
+
+// Simple but complex logic. i can pass a message "Something went wrong, statusCode: 200, 201,301, 302, 400, 401, 404" instead of hard coding the message and status code there.
+// and filter out the statusCode using string methods. and also configure the error message by taking out the status code and displaying the rest
+// With this logic implemented. there won't be any error handling function inside the handleAsyncErr wherever it is being used
+// Can be implemented but i will leave the idea for now. Maybe later

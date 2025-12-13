0f084faf48fadf81dd7e86f935bea1a50c8e5bce
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-12-12 16:11:44 -0800

    Protecting routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { createUser, getCurrentUser } from "../Controllers/UsersController";
 import signUpAuth from "../middlewares/signUp_middleware";
 import loginAuth from "../middlewares/login_middleware";
+import { protectRoutes } from "../Controllers/AuthController";
 
 const userRouter = Router();
 
 userRouter.post("/signup", signUpAuth, createUser);
 
-userRouter.post("/login", loginAuth, getCurrentUser);
+userRouter.post("/login", protectRoutes, loginAuth, getCurrentUser);
 
 export default userRouter;

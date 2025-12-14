9eb8567662ab9c80f157baba7458694b9c21b71d
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-12-14 09:20:26 -0800

    Testing protection complete. time to test other endpoints. there is nothing to protect on login and signup

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,12 +2,11 @@ import { Router } from "express";
 import { createUser, getCurrentUser } from "../Controllers/UsersController";
 import signUpAuth from "../middlewares/signUp_middleware";
 import loginAuth from "../middlewares/login_middleware";
-import { protectRoutes } from "../Controllers/AuthController";
 
 const userRouter = Router();
 
-userRouter.post("/signup", protectRoutes, signUpAuth, createUser);
+userRouter.post("/signup", signUpAuth, createUser);
 
-userRouter.post("/login", protectRoutes, loginAuth, getCurrentUser);
+userRouter.post("/login", loginAuth, getCurrentUser);
 
 export default userRouter;

d54f47a7c97a74678076f2dbe37dc68d870c6761
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-12-24 23:02:14 -0800

    Safeguarding reset password routes with multiples layers of protections

--- src/middlewares/user_middleware.ts
+++ src/middlewares/user_middleware.ts
@@ -1,13 +1,19 @@
 import type { NextFunction, Request, Response } from "express";
-import validator from "validator";
 import { parsePhoneNumberWithError } from "libphonenumber-js";
+import validator from "validator";
 import { CheckBool } from "../Controllers/CheckBool";
 import catchError from "../Utils/CatchError";
 import type { BodyCasting } from "../Utils/DataChecking";
+import { verifyForgetPasswordToken } from "../Utils/TokenHashing";
 
-
-export const loginAuth = async (req: Request, res: Response, next: NextFunction) => {
+export const loginAuth = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
 	try {
+		if (!req.body) throw new Error("Sorry missing something");
+
 		const { email, password }: BodyCasting = req.body;
 
 		// This is unnecessary because this error checking will be handled on the frontend but it's better to be on the safe side. leave it this way.
@@ -27,7 +33,8 @@ export const loginAuth = async (req: Request, res: Response, next: NextFunction)
 		const { errState, errMsg } = CheckBool(check2);
 		if (!errState) throw new Error(`Login-Msg: ${errMsg}`);
 		res.locals.loginData = arrLow;
-	} catch (error: any) { // Adding any as the type works in development but it would be advisable to make some adjustment for important error incoming
+	} catch (error: any) {
+		// Adding any as the type works in development but it would be advisable to make some adjustment for important error incoming
 		catchError(error, error.message, 400, next);
 		return;
 	}
@@ -35,9 +42,13 @@ export const loginAuth = async (req: Request, res: Response, next: NextFunction)
 	next();
 };
 
-
-export const signUpAuth = async (req: Request, res: Response, next: NextFunction) => {
+export const signUpAuth = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
 	try {
+		if (!req.body) throw new Error("Sorry missing something");
 		const { name, email, phone, password, passwordConfirm }: BodyCasting =
 			req.body;
 
@@ -80,32 +91,72 @@ export const signUpAuth = async (req: Request, res: Response, next: NextFunction
 	next();
 };
 
-export const forgotPasswordAuth = (req: Request, res: Response, next: NextFunction) => {
-       try {
-		const { email }: BodyCasting = req.body;
+export const forgotPasswordAuth = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		if (req.method === "POST") {
+			if (!req.body) throw new Error("Sorry missing something");
 
-		// This is unnecessary because this error checking will be handled on the frontend but it's better to be on the safe side. leave it this way.
-		if (!email)
-			throw new Error("Sorry you can't leave any field empty");
+			const { email }: BodyCasting = req.body;
 
-		// Turning all input data to lowercase and Filtering Signs and symbols
+			// This is unnecessary because this error checking will be handled on the frontend but it's better to be on the safe side. leave it this way.
+			if (!email) throw new Error("Sorry you can't leave any field empty");
 
-		const arrLow: Array<string> = [email].map((e) =>
-			e.toLowerCase().replace(/[^a-zA-Z0-9@.]/g, ""),
-		);
+			// Turning all input data to lowercase and Filtering Signs and symbols
 
-		const check2 = validator.isEmail(email)
-			? { errorState: true }
-			: { errorState: false, errorMessage: "Sorry, Not a valid Email Address" };
+			const arrLow: Array<string> = [email].map((e) =>
+				e.toLowerCase().replace(/[^a-zA-Z0-9@.]/g, ""),
+			);
 
-		const { errState, errMsg } = CheckBool(check2);
-		if (!errState) throw new Error(`Forgot-Password-Msg: ${errMsg}`);
-		res.locals.forgotPasswordData = arrLow;
-	} catch (error: any) { // Adding any as the type works in development but it would be advisable to make some adjustment for important error incoming
+			const check2 = validator.isEmail(email)
+				? { errorState: true }
+				: {
+						errorState: false,
+						errorMessage: "Sorry, Not a valid Email Address",
+					};
+
+			const { errState, errMsg } = CheckBool(check2);
+			if (!errState) throw new Error(`Forgot-Password-Msg: ${errMsg}`);
+			res.locals.forgotPasswordData = arrLow;
+		} else if (req.method === "PATCH") {
+			if (!req.session.resetToken || !req.body)
+				throw new Error("Sorry missing something");
+			const { newPassword, newConfirmPassword } = req.body;
+
+			const paramToken = req.params.token;
+			const { resetPasswordTokenExpires, saltToken, id, resetUrl } =
+				req.session.resetToken;
+			const check3 =
+				resetPasswordTokenExpires < new Date()
+					? { errorState: false, errorMessage: "Token Expires" }
+					: { errorState: true };
+			const check4 =
+				verifyForgetPasswordToken(id, saltToken, paramToken) === false
+					? { errorState: false, errorMessage: "Token not correct" }
+					: { errorState: true };
+			const check5 =
+				newPassword !== newConfirmPassword
+					? { errorState: false, errorMessage: "Passwords does not match" }
+					: { errorState: true };
+			const { errState, errMsg } = CheckBool(check3, check4, check5);
+			if (!errState) throw new Error(`Error1: ${errMsg}`);
+			// if(resetUrl !== req.get("Referrer")) throw new Error("Not authorize to access this page"); this is for production or after the frontend is ready
+
+			const arrLow: Array<string> = [newPassword].map((e) =>
+				e.toLowerCase().replace(/[^a-zA-Z0-9@.]/g, ""),
+			);
+			res.locals.newPasswordData = arrLow;
+		} else {
+			throw new Error("Something went wrong. please follow normal routes");
+		}
+	} catch (error: any) {
+		// Adding any as the type works in development but it would be advisable to make some adjustment for important error incoming
 		catchError(error, error.message, 400, next);
 		return;
 	}
 
-	next()
-}
-
+	next();
+};

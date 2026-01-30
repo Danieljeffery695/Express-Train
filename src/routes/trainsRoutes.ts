6e6cf2f9706f9739995b4856d16a68990784413f
Danieljeffery695 <danielgodfrey695@gmail.com>
2026-01-30 12:01:41 -0800

    Route for getting all trains

--- src/routes/trainsRoutes.ts
+++ src/routes/trainsRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createTrain } from "../Controllers/TrainController";
+import { createTrain, getAllTrain } from "../Controllers/TrainController";
 import {create_Train_Auth} from "../middlewares/train_middleware";
 
 const trainRouter = Router();
 
 trainRouter.post("/api/v1/createTrain", create_Train_Auth, createTrain);
+
+trainRouter.get("/api/v1/getTrains", getAllTrain)
 
 export default trainRouter;

c00c680e59182f03aa18e4d35ac87655b4afe1df
Danieljeffery695 <danielgodfrey695@gmail.com>
2026-02-07 23:48:01 -0800

    pre middleware for findOneAndDelete for train model

--- src/Models/Trains.ts
+++ src/Models/Trains.ts
@@ -1,5 +1,8 @@
 import mongoose, { Schema } from "mongoose";
 import type { ITrainCreate } from "../Utils/DataChecking";
+import TrainCoach from "./Coaches";
+import TrainStation from "./Stations";
+import TrainRoute from "./TrainRoute";
 
 const trainSchema = new mongoose.Schema<ITrainCreate>(
 	{
@@ -33,6 +36,29 @@ const trainSchema = new mongoose.Schema<ITrainCreate>(
 		timestamps: true,
 	},
 );
+
+trainSchema.pre("findOneAndDelete", async function () {
+	// Deleting Train Route
+	const doc = (await this.model.findOne(
+		this.getFilter(),
+	)) as ITrainCreate | null;
+
+	if (doc?.route) {
+		const train_Route_Delete_Doc = await TrainRoute.findByIdAndDelete(
+			doc.route,
+			{ new: true },
+		);
+
+		//   Deleting Train Station
+		if (train_Route_Delete_Doc)
+			await TrainStation.findByIdAndDelete(train_Route_Delete_Doc.from);
+	}
+
+	// Deleting Train coaches
+	if (doc?.coaches) {
+		await TrainCoach.findByIdAndDelete(doc.coaches);
+	}
+});
 
 const Trains = mongoose.model<ITrainCreate>("Trains", trainSchema);
 

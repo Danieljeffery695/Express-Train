1a44fda1944d607da04ff3b2441efca3096ce31d
Danieljeffery695 <danielgodfrey695@gmail.com>
2026-02-08 22:47:26 -0800

    creating train delete route

--- src/routes/trainsRoutes.ts
+++ src/routes/trainsRoutes.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
-import { createTrain, getAllTrain, updateTrain } from "../Controllers/TrainController";
-import {create_Train_Auth, update_Train_Auth} from "../middlewares/train_middleware";
+import {
+	createTrain,
+	deleteTrain,
+	getAllTrain,
+	updateTrain,
+} from "../Controllers/TrainController";
+import {
+	create_Train_Auth,
+	update_Train_Auth,
+} from "../middlewares/train_middleware";
 
 const trainRouter = Router();
 
 trainRouter.post("/api/v1/createTrain", create_Train_Auth, createTrain);
 
-trainRouter.get("/api/v1/getTrains", getAllTrain)
-.patch("/api/v1/updateTrain/:id", update_Train_Auth, updateTrain);
+trainRouter
+	.get("/api/v1/getTrains", getAllTrain)
+	.patch("/api/v1/updateTrain/:id", update_Train_Auth, updateTrain)
+	.delete("/api/v1/deleteTrain/:id", deleteTrain);
 
 export default trainRouter;
